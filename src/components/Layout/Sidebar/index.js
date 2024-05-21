7187c9afc5c239b11fc9f5a5ca594146f7253592
Roshan Sandaruwan <112076077+Roshan-Sandaruwan@users.noreply.github.com>
2024-05-21 18:59:32 +0530

    side bar added

---
+++ src/components/Layout/Sidebar/index.js
@@ -0,0 +1,39 @@
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+import LogoS from '../../../assets/images/logo-s.png'
+import LogoSubtitile from '../../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+
+
+const Sidebar = () => {
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="logo" />
+        <img className="sub-logo" src={LogoSubtitile} alt="logo" />
+      </Link>
+
+      <nav>
+
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+
+      <NavLink exact="true" activeclassname="active" to="/about" className="about-link" >
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+
+      <NavLink exact="true" activeclassname="active" to="/contact" className="contact-link" >
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+      </NavLink>
+
+      </nav>
+
+
+
+    </div>
+  )
+}
+
+export default Sidebar
