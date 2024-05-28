dc62697e96a08380f7b4802a6a150c84a7b55777
Roshan Sandaruwan <112076077+Roshan-Sandaruwan@users.noreply.github.com>
2024-05-28 18:50:09 +0530

    Home page complete without right side animation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,21 +1,66 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import LogoTitle from '../../assets/images/logo-R.png'
 import { Link } from 'react-router-dom'
-import './index.scss';
+import './index.scss'
+import AnimatedLetters from '../AnimatedLetters'
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['o', 's', 'h', 'a', 'n']
+  const jobArray = [
+    'W',
+    'e',
+    'b',
+    ' ',
+    'D',
+    'e',
+    'v',
+    'e',
+    'l',
+    'o',
+    'p',
+    'e',
+    'r',
+    '.',
+  ]
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 4000);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
   return (
-    <div className='container home-page'>
-      <div className='text-zone'>
-        <h1>Hi, <br/> I'm
-        <img src={LogoTitle} alt='developer' />
-        oshan
-        <br/>
-        Web Developer
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
+          <br /> 
+
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          
+          <img src={LogoTitle} alt="developer" />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
         </h1>
 
         <h2>Frontend Developer / Content Creator </h2>
-        <Link to= "/contact" className='flat-button' >CONTACT ME</Link>
+        <Link to="/contact" className="flat-button">
+          CONTACT ME
+        </Link>
       </div>
     </div>
   )
